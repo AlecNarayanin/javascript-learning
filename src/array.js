38a8d7cb662738b46effae55473f079035ee7bc6
Alec Narayanin <narayanin.alec@hotmail.com>
2022-03-22 14:49:41 +0100

    Correction énoncé

--- src/array.js
+++ src/array.js
@@ -31,15 +31,13 @@ const panier = [
 
 //TODO: Récupérer le premier article à 300$
 
-//TODO: Afficher l'ensemble des noms d'article dans la consolet
+//TODO: Afficher l'ensemble des noms d'article dans la console
 
 //TODO: Affirmer s'il existe des articles avec un prix supérieur à 600$
 
 //TODO: Affirmer si tous les articles sont supérieur à 100$
 
 //TODO: Calculer le prix total du panier
-
-//TODO: Affirmer si une lampe est inclus dans le panier
 
 const simpleArray = [1, 3, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
